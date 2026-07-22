1f91133ab87178a6ba0205bdaedc674e58338f07
Sauzan Faza <sauzanfaza@gmail.com>
2026-07-22 13:06:12 +0700

    ubah database menggunakan laragon

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,48 +1,27 @@
-const mysql = require('mysql2')
+const mysql = require('mysql2');
+const { Connection } = require('mysql2/promise');
 require('dotenv').config();
 
-// console.log(process.env.DB_HOST);
-// console.log(process.env.DB_USER);
-// console.log(process.env.DB_NAME);
-
-// const db = mysql.createPool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0
-// })
-
 const db = mysql.createPool({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "umkm_db",
-});
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+})
 
-db.getConnection((err, conn) => {
-  if (err) {
-    console.log("Gagal connect");
-    console.log(err);
-    return;
+// cek koneksi 
+db.getConnection((err, connection) => {
+  if(err) {
+    console.log("Gagal connect euy!!")
+    console.log(err)
+  } else {
+    console.log("MySQL dah connect yeayy")
+    connection.release()
   }
-
-  console.log("Connected!");
-  conn.release();
-});
-
-
-
-
-// db.connect((err) => {
-//     if(err) {
-//         console.log('DB Error: ', err)
-//     } else {
-//         console.log("MySQL Connected")
-//     }
-// }) klo pke pool ini gk perrlu
+})
 
 module.exports = db
